97813412f7933b7fdd41b4a5d0595fb20a5704af
Jim Nasby <decibel@decina.local>
2014-10-17 19:19:46 -0500

    Tests actually work now

--- test/sql/base.sql
+++ test/sql/base.sql
@@ -3,4 +3,16 @@ BEGIN;
 \i test/pgtap-core.sql
 \i sql/variant.sql
 
+\pset format unaligned
+\pset tuples_only true
+\pset pager
+-- Revert all changes on failure.
+\set ON_ERROR_ROLLBACK 1
+\set ON_ERROR_STOP true
+
+SELECT plan(1);
+
+SELECT ok(true);
+
+SELECT finish();
 ROLLBACK;
