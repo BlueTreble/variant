f52e250fb492b01d4ef11cc213ea3ebecb62df87
Jim Nasby <Jim.Nasby@BlueTreble.com>
2014-11-17 17:30:03 -0600

    Very basic test file

--- test/sql/base.sql
+++ test/sql/base.sql
@@ -3,12 +3,18 @@ BEGIN;
 \i test/pgtap-core.sql
 \i sql/variant.sql
 
+SET client_min_messages = DEBUG;
+SELECT '{text,test}'::variant.variant;
+SELECT '{text,test}'::variant.variant::text;
+
 \pset format unaligned
 \pset tuples_only true
 \pset pager
 -- Revert all changes on failure.
 \set ON_ERROR_ROLLBACK 1
 \set ON_ERROR_STOP true
+
+\quit
 
 SELECT plan(1);
 
