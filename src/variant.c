42176d182145f27fe7eb67d395f4cff69fd812cb
Jim Nasby <Jim.Nasby@BlueTreble.com>
2014-11-22 23:34:37 -0600

    More cleanup

--- src/variant.c
+++ src/variant.c
@@ -47,33 +47,28 @@ PG_FUNCTION_INFO_V1(variant_in);
 Datum
 variant_in(PG_FUNCTION_ARGS)
 {
-	char					 *input = PG_GETARG_CSTRING(0);
+	char						*input = PG_GETARG_CSTRING(0);
 	Datum						composite;
 	HeapTupleHeader	composite_tuple;
 	bool						isnull;
 	Oid							intTypeOid = InvalidOid;
 	int32						typmod = 0;
-	Oid							typiofunc;
-	Oid							typioparam;
-	FmgrInfo	 			proc;
 	Oid							orgTypeOid;
-	char					 *orgTypeName;
-	text					 *orgData;
-	StringInfoData	cmd;
+	char						*orgTypeName;
+	text						*orgData;
 	bool						do_pop = false;
-	int							ret;
 	Size						len;
 	Variant					var;
-	bytea					 *result;
-	char					 *tmp;
+	char						*tmp;
 	Datum					  tmpDatum;
 	Datum					  org_datum;
-	int16 					resultTypLen;
-	bool 						resultTypByVal;
-	int16		typlen;
-	bool		typbyval;
-	char		typalign;
-	char		typdelim;
+	FmgrInfo	 			proc;
+	int16 					typLen;
+	bool 						typByVal;
+	char						typAlign;
+	char						typDelim;
+	Oid							typIoParam;
+	Oid							typIoFunc;
 
 	if (!SPI_connect()) {
 		do_pop = true;
@@ -83,107 +78,54 @@ variant_in(PG_FUNCTION_ARGS)
 	intTypeOid = getIntOid();
 
 	/* Cast input data to our internal composite type */
-	getTypeInputInfo(intTypeOid, &typiofunc, &typioparam);
-	fmgr_info_cxt(typiofunc, &proc, fcinfo->flinfo->fn_mcxt);
-	composite=InputFunctionCall(&proc, input, typioparam, typmod);
-
-	tmp=get_cstring(intTypeOid, fcinfo->flinfo->fn_mcxt, composite);
-	elog(LOG, "composite: %s, size %u", tmp, VARSIZE(composite));
+	getTypeInputInfo(intTypeOid, &typIoFunc, &typIoParam);
+	fmgr_info_cxt(typIoFunc, &proc, fcinfo->flinfo->fn_mcxt);
+	composite=InputFunctionCall(&proc, input, typIoParam, typmod);
 
 	/* Extract data from internal composite type */
 	composite_tuple=DatumGetHeapTupleHeader(composite);
-
-	tmp=get_cstring(intTypeOid, fcinfo->flinfo->fn_mcxt, composite);
-	elog(LOG, "(composite_tuple) composite: %s, size %u", tmp, VARSIZE(composite));
-
-	tmpDatum=PointerGetDatum(composite_tuple);
-	tmp=get_cstring(intTypeOid, fcinfo->flinfo->fn_mcxt, tmpDatum);
-	elog(LOG, "composite_tuple size %u, tmpDatum: %s, size %u", VARSIZE(composite_tuple), tmp, VARSIZE(tmp));
-
 	orgTypeOid = (Oid) GetAttributeByNum( composite_tuple, 1, &isnull );
-
-	tmp=get_cstring(intTypeOid, fcinfo->flinfo->fn_mcxt, composite);
-	elog(LOG, "(orgTypeOid) composite: %s, size %u", tmp, VARSIZE(composite));
 
 	if (isnull)
 		elog(ERROR, "original_type of variant must not be NULL");
 
 	orgTypeName = format_type_be(orgTypeOid);
 	orgData = (text *) GetAttributeByNum( composite_tuple, 2, &isnull );
-	elog(LOG, "orgData: %s, size %u", text_to_cstring(orgData), VARSIZE(orgData));
-
-	tmp=get_cstring(intTypeOid, fcinfo->flinfo->fn_mcxt, composite);
-	elog(LOG, "(orgData) composite: %s, size %u", tmp, VARSIZE(composite));
-
-
-	/* Make sure we can legitimately cast our input to the passed in type */
-	initStringInfo(&cmd);
-
-	/*
-	 * $1 is our raw input. Cast it to internal composite, extract data
-	 * portion, and attempt cast.
-	 */
-	appendStringInfo(&cmd, "SELECT CAST( '$1' AS %s )", orgTypeName);
-
-	if (!(ret = SPI_execute_with_args(cmd.data, 1, &orgTypeOid, (Datum *) &orgData, isnull ? "n" : " ", true, 1)))
-		elog(ERROR, "SPI_execute_with_args returned %s", SPI_result_code_string(ret));
-
-	/*
-	 * We can't just hand back a raw HeapTupleHeader Datum; it will confuse other
-	 * parts of the system. So treat it as a bytea instead.
-	 */
-	len = VARSIZE(composite);
-	elog( LOG, "len %zu, VARHDRSZ %u, VARSIZE %u, HeapTupleHeaderGetDatumLength %u", len, VARHDRSZ, VARSIZE(composite), HeapTupleHeaderGetDatumLength(composite));
-	tmpDatum=PointerGetDatum(composite_tuple);
-	tmp=get_cstring(intTypeOid, fcinfo->flinfo->fn_mcxt, tmpDatum);
-	elog(LOG, "composite_tuple size %u, tmpDatum: %s, size %u", VARSIZE(composite_tuple), tmp, VARSIZE(tmp));
-	result = (bytea *) palloc0(len + VARHDRSZ);
-	SET_VARSIZE(result, len + VARHDRSZ);
-	memcpy(VARDATA(result), &composite, len);
-	elog(LOG, "result size %u", VARSIZE(result));
 
 	get_type_io_data(orgTypeOid,
 						 IOFunc_input,
-						 &resultTypLen,
-						 &resultTypByVal,
-						 &typalign,
-						 &typdelim,
-						 &typioparam,
-						 &typiofunc);
+						 &typLen,
+						 &typByVal,
+						 &typAlign,
+						 &typDelim,
+						 &typIoParam,
+						 &typIoFunc);
 
 
-	fmgr_info_cxt(typiofunc, &proc, fcinfo->flinfo->fn_mcxt);
-	org_datum=InputFunctionCall(&proc, text_to_cstring(orgData), typioparam, typmod);
-	/*
-	get_typlenbyval(orgTypeOid, &resultTypLen, &resultTypByVal);
-	*/
-	if (resultTypLen == -1)
-		tmpDatum = PointerGetDatum(PG_DETOAST_DATUM_COPY(org_datum));
-	else
-		tmpDatum = datumCopy(org_datum, resultTypByVal, resultTypLen);
-	len = datumGetSize(tmpDatum, resultTypByVal, resultTypLen);
+	fmgr_info_cxt(typIoFunc, &proc, fcinfo->flinfo->fn_mcxt);
+	org_datum=InputFunctionCall(&proc, text_to_cstring(orgData), typIoParam, typmod);
+	tmpDatum = datumCopy(org_datum, typByVal, typLen);
+	len = datumGetSize(tmpDatum, typByVal, typLen);
 
 	var = (Variant) MemoryContextAllocZero(fcinfo->flinfo->fn_mcxt, len + VHDRSZ);
-	Pointer ptr;
-	ptr = VDATAPTR(var);
-	Pointer ptr2 = (Pointer) att_align_pointer(ptr, typalign, resultTypLen, ptr);
-	if( ptr != ptr2 )
-		elog(ERROR, "ptr %p doesn't align to %p", ptr, ptr2);
-
 	SET_VARSIZE(var, len + VHDRSZ);
 	var->typeOid = orgTypeOid;
 
-	elog(LOG, "VDATAPTR %p, tmpDatum %p, len %zu, VHDRSZ %zu", (void *)VDATAPTR(var), (void *)tmpDatum, len, VHDRSZ);
-	memcpy((char *)VDATAPTR(var), (char *)tmpDatum, len);
-	text *tmptext=(text *)VDATAPTR(var);
-	elog(LOG, "text %s", text_to_cstring(tmptext));
-	/*
+	Pointer ptr;
+	ptr = VDATAPTR(var);
 
-	elog(LOG, "var %p, var->data %p, tmpDatum %p, len %zu, VHDRSZ %zu", var, &(var->data), (void *)tmpDatum, len, VHDRSZ);
-	memcpy(&(var->data), &tmpDatum, len);
-	text *tmptext=(text *)var->data;
-	elog(LOG, "tmptext %p", tmptext);
-	*/
+	/* We don't know what we could get passed, so best not to make this an asert */
+	Pointer ptr2 = (Pointer) att_align_pointer(ptr, typAlign, typLen, ptr);
+	if( ptr != ptr2 )
+		elog(ERROR, "ptr %p doesn't align to %p", ptr, ptr2);
+
+	if (typByVal) {
+		elog(LOG, "VDATAPTR %p, tmpDatum %p, len %zu, VHDRSZ %zu", (void *)VDATAPTR(var), &tmpDatum, len, VHDRSZ);
+		memcpy(ptr, &tmpDatum, len);
+	} else {
+		elog(LOG, "VDATAPTR %p, tmpDatum %p, len %zu, VHDRSZ %zu", (void *)VDATAPTR(var), (Pointer) tmpDatum, len, VHDRSZ);
+		memcpy(ptr, (Pointer) tmpDatum, len);
+	}
 
   if (do_pop)
 		SPI_pop();
@@ -197,12 +139,12 @@ char *
 get_cstring(Oid intTypeOid, MemoryContext ctx, Datum dat)
 {
 	char					 *output;
-	Oid							typiofunc;
+	Oid							typIoFunc;
 	bool						isvarlena;
 	FmgrInfo	 			proc;
 
-	getTypeOutputInfo(intTypeOid, &typiofunc, &isvarlena);
-	fmgr_info_cxt(typiofunc, &proc, ctx);
+	getTypeOutputInfo(intTypeOid, &typIoFunc, &isvarlena);
+	fmgr_info_cxt(typIoFunc, &proc, ctx);
 	output = OutputFunctionCall(&proc, dat);
 
 	return output;
@@ -215,18 +157,36 @@ variant_out(PG_FUNCTION_ARGS)
 	Datum						input_datum = PG_GETARG_DATUM(0);
 	Variant					input = (Variant) PG_DETOAST_DATUM_COPY(input_datum);
 	Oid							intTypeOid = InvalidOid;
-	Oid							typiofunc;
+	Oid							orgTypeOid = input->typeOid;
+	Datum						orig_datum;
 	bool						isvarlena;
 	bool						need_quote;
 	FmgrInfo	 			proc;
-	char					 *tmp;
-	char					 *org_cstring;
+	char						*tmp;
+	char						*org_cstring;
 	StringInfoData	out;
+	int16 					typLen;
+	bool 						typByVal;
+	char						typAlign;
+	char						typDelim;
+	Oid							typIoParam;
+	Oid							typIoFunc;
 
 	/* Get cstring of original data */
-	getTypeOutputInfo(input->typeOid, &typiofunc, &isvarlena);
-	fmgr_info_cxt(typiofunc, &proc, fcinfo->flinfo->fn_mcxt);
-	org_cstring = OutputFunctionCall(&proc, VDATAPTR(input));
+	get_type_io_data(orgTypeOid,
+						 IOFunc_output,
+						 &typLen,
+						 &typByVal,
+						 &typAlign,
+						 &typDelim,
+						 &typIoParam,
+						 &typIoFunc);
+	fmgr_info_cxt(typIoFunc, &proc, fcinfo->flinfo->fn_mcxt);
+	if( typByVal )
+		orig_datum = *VDATAPTR(input);
+	else
+		orig_datum = VDATAPTR(input);
+	org_cstring = OutputFunctionCall(&proc, orig_datum);
 
 	/*
 	 * Detect whether we need double quotes for this value
