41626587cf2d6aca9aea1739eb95e44b82027a3e
Jim Nasby <Jim.Nasby@BlueTreble.com>
2014-11-23 02:01:53 -0600

    Start using a cache

--- src/variant.c
+++ src/variant.c
@@ -12,8 +12,20 @@
 #include "utils/lsyscache.h"
 #include "variant.h"
 
-static Oid getIntOid();
-static char * get_cstring(Oid intTypeOid, MemoryContext ctx, Datum dat);
+/* fn_extra cache entry */
+typedef struct VariantCache
+{
+	FmgrInfo	proc;				/* lookup result for typiofunc */
+	/* Information for original type */
+	Oid				typId;
+	Oid				typIoParam;
+	int16 		typLen;
+	bool 			typByVal;
+	char			typAlign;
+	char			*outString;	/* Initial part of output string. NULL for input functions. */
+} VariantCache;
+
+
 typedef struct {
 	int32		vl_len_;		/* varlena header (do not touch directly!) */
 	Oid		typeOid;			/* OID of original data type */
@@ -23,6 +35,10 @@ typedef VariantData *Variant;
 
 #define VHDRSZ				(sizeof(VariantData))
 #define VDATAPTR(x)		( (Pointer) ( (x) + 1 ) )
+
+static VariantCache * get_cache(FunctionCallInfo fcinfo, Oid origTypId, IOFuncSelector func);
+static Oid getIntOid();
+static char * get_cstring(Oid intTypeOid, MemoryContext ctx, Datum dat);
 
 
 /*
@@ -47,65 +63,45 @@ PG_FUNCTION_INFO_V1(variant_in);
 Datum
 variant_in(PG_FUNCTION_ARGS)
 {
-	char						*input = PG_GETARG_CSTRING(0);
-	Datum						composite;
-	HeapTupleHeader	composite_tuple;
-	bool						isnull;
-	Oid							intTypeOid = InvalidOid;
-	int32						typmod = 0;
-	Oid							orgTypeOid;
-	char						*orgTypeName;
-	text						*orgData;
-	bool						do_pop = false;
-	Size						len;
-	Variant					var;
-	char						*tmp;
-	Datum					  tmpDatum;
-	Datum					  org_datum;
-	FmgrInfo	 			proc;
-	int16 					typLen;
-	bool 						typByVal;
-	char						typAlign;
-	char						typDelim;
-	Oid							typIoParam;
-	Oid							typIoFunc;
+	VariantCache	*cache;
+	char					*input = PG_GETARG_CSTRING(0);
+	bool					isnull;
+	Oid						intTypeOid = InvalidOid;
+	int32					typmod = 0;
+	Oid						orgTypeOid;
+	text					*orgData;
+	bool					do_pop = false;
+	Size					len;
+	Variant				var;
+	Datum				  tmpDatum;
+	Datum				  org_datum;
 
-	if (!SPI_connect()) {
-		do_pop = true;
-		SPI_push();
-	}
+	/* Eventually getting rid of this crap, so segregate it */
+		intTypeOid = getIntOid();
 
-	intTypeOid = getIntOid();
+		FmgrInfo	 		proc;
+		Datum						composite;
+		HeapTupleHeader	composite_tuple;
+		Oid							typIoParam;
+		Oid							typIoFunc;
 
-	/* Cast input data to our internal composite type */
-	getTypeInputInfo(intTypeOid, &typIoFunc, &typIoParam);
-	fmgr_info_cxt(typIoFunc, &proc, fcinfo->flinfo->fn_mcxt);
-	composite=InputFunctionCall(&proc, input, typIoParam, typmod);
+		/* Cast input data to our internal composite type */
+		getTypeInputInfo(intTypeOid, &typIoFunc, &typIoParam);
+		fmgr_info_cxt(typIoFunc, &proc, fcinfo->flinfo->fn_mcxt);
+		composite=InputFunctionCall(&proc, input, typIoParam, typmod);
 
-	/* Extract data from internal composite type */
-	composite_tuple=DatumGetHeapTupleHeader(composite);
-	orgTypeOid = (Oid) GetAttributeByNum( composite_tuple, 1, &isnull );
+		/* Extract data from internal composite type */
+		composite_tuple=DatumGetHeapTupleHeader(composite);
+		orgTypeOid = (Oid) GetAttributeByNum( composite_tuple, 1, &isnull );
+		if (isnull)
+			elog(ERROR, "original_type of variant must not be NULL");
+		orgData = (text *) GetAttributeByNum( composite_tuple, 2, &isnull );
+	/* End crap */
 
-	if (isnull)
-		elog(ERROR, "original_type of variant must not be NULL");
-
-	orgTypeName = format_type_be(orgTypeOid);
-	orgData = (text *) GetAttributeByNum( composite_tuple, 2, &isnull );
-
-	get_type_io_data(orgTypeOid,
-						 IOFunc_input,
-						 &typLen,
-						 &typByVal,
-						 &typAlign,
-						 &typDelim,
-						 &typIoParam,
-						 &typIoFunc);
-
-
-	fmgr_info_cxt(typIoFunc, &proc, fcinfo->flinfo->fn_mcxt);
-	org_datum=InputFunctionCall(&proc, text_to_cstring(orgData), typIoParam, typmod);
-	tmpDatum = datumCopy(org_datum, typByVal, typLen);
-	len = datumGetSize(tmpDatum, typByVal, typLen);
+	cache = get_cache(fcinfo, orgTypeOid, IOFunc_input);
+	org_datum=InputFunctionCall(&cache->proc, text_to_cstring(orgData), cache->typIoParam, typmod); // TODO: Use real original typmod
+	tmpDatum = datumCopy(org_datum, cache->typByVal, cache->typLen);
+	len = datumGetSize(tmpDatum, cache->typByVal, cache->typLen);
 
 	var = (Variant) MemoryContextAllocZero(fcinfo->flinfo->fn_mcxt, len + VHDRSZ);
 	SET_VARSIZE(var, len + VHDRSZ);
@@ -115,22 +111,19 @@ variant_in(PG_FUNCTION_ARGS)
 	ptr = VDATAPTR(var);
 
 	/* We don't know what we could get passed, so best not to make this an asert */
-	Pointer ptr2 = (Pointer) att_align_pointer(ptr, typAlign, typLen, ptr);
-	if( ptr != ptr2 )
-		elog(ERROR, "ptr %p doesn't align to %p", ptr, ptr2);
+	{
+		Pointer ptr2 = (Pointer) att_align_pointer(ptr, cache->typAlign, cache->typLen, ptr);
+		if( ptr != ptr2 )
+			elog(ERROR, "ptr %p doesn't align to %p", ptr, ptr2);
+	}
 
-	if (typByVal) {
+	if (cache->typByVal) {
 		elog(LOG, "VDATAPTR %p, tmpDatum %p, len %zu, VHDRSZ %zu", (void *)VDATAPTR(var), &tmpDatum, len, VHDRSZ);
 		memcpy(ptr, &tmpDatum, len);
 	} else {
 		elog(LOG, "VDATAPTR %p, tmpDatum %p, len %zu, VHDRSZ %zu", (void *)VDATAPTR(var), (Pointer) tmpDatum, len, VHDRSZ);
 		memcpy(ptr, (Pointer) tmpDatum, len);
 	}
-
-  if (do_pop)
-		SPI_pop();
-	else
-		SPI_finish();
 
 	PG_RETURN_POINTER(var);
 }
@@ -154,39 +147,22 @@ PG_FUNCTION_INFO_V1(variant_out);
 Datum
 variant_out(PG_FUNCTION_ARGS)
 {
-	Datum						input_datum = PG_GETARG_DATUM(0);
-	Variant					input = (Variant) PG_DETOAST_DATUM_COPY(input_datum);
-	Oid							intTypeOid = InvalidOid;
-	Oid							orgTypeOid = input->typeOid;
-	Datum						orig_datum;
-	bool						isvarlena;
-	bool						need_quote;
-	FmgrInfo	 			proc;
-	char						*tmp;
-	char						*org_cstring;
+	VariantCache	*cache;
+	Datum					input_datum = PG_GETARG_DATUM(0);
+	Variant				input = (Variant) PG_DETOAST_DATUM_COPY(input_datum);
+	Oid						orgTypeOid = input->typeOid;
+	Datum					orig_datum;
+	bool					need_quote;
+	char					*tmp;
+	char					*org_cstring;
 	StringInfoData	out;
-	int16 					typLen;
-	bool 						typByVal;
-	char						typAlign;
-	char						typDelim;
-	Oid							typIoParam;
-	Oid							typIoFunc;
 
-	/* Get cstring of original data */
-	get_type_io_data(orgTypeOid,
-						 IOFunc_output,
-						 &typLen,
-						 &typByVal,
-						 &typAlign,
-						 &typDelim,
-						 &typIoParam,
-						 &typIoFunc);
-	fmgr_info_cxt(typIoFunc, &proc, fcinfo->flinfo->fn_mcxt);
-	if( typByVal )
+	cache = get_cache(fcinfo, orgTypeOid, IOFunc_output);
+	if( cache->typByVal )
 		orig_datum = *VDATAPTR(input);
 	else
-		orig_datum = VDATAPTR(input);
-	org_cstring = OutputFunctionCall(&proc, orig_datum);
+		orig_datum = (Datum) VDATAPTR(input);
+	org_cstring = OutputFunctionCall(&cache->proc, orig_datum);
 
 	/*
 	 * Detect whether we need double quotes for this value
@@ -212,11 +188,7 @@ variant_out(PG_FUNCTION_ARGS)
 
 	/* And emit the string */
 	initStringInfo(&out);
-
-	/* format_type_by handles quoting for us */
-	appendStringInfoChar(&out, '(');
-	appendStringInfoString(&out, format_type_be(input->typeOid));
-	appendStringInfoChar(&out, ',');
+	appendStringInfoString(&out, cache->outString);
 
 	if (!need_quote)
 		appendStringInfoString(&out, org_cstring);
@@ -239,11 +211,66 @@ variant_out(PG_FUNCTION_ARGS)
 	PG_RETURN_CSTRING(out.data);
 }
 
+/*
+ * get_cache: get/set cached info
+ */
+static VariantCache *
+get_cache(FunctionCallInfo fcinfo, Oid origTypId, IOFuncSelector func)
+{
+	VariantCache *cache = (VariantCache *) fcinfo->flinfo->fn_extra;
+
+	if (cache == NULL || cache->typId != origTypId)
+	{
+		char						typDelim;
+		Oid							typIoFunc;
+
+		/*
+		 * We can get different OIDs in one call, so don't needlessly Alloc
+		 */
+		if (cache == NULL)
+			cache = (VariantCache *) MemoryContextAlloc(fcinfo->flinfo->fn_mcxt,
+												   sizeof(VariantCache));
+
+		cache->typId = origTypId;
+
+		get_type_io_data(cache->typId,
+							 func,
+							 &cache->typLen,
+							 &cache->typByVal,
+							 &cache->typAlign,
+							 &typDelim,
+							 &cache->typIoParam,
+							 &typIoFunc);
+		fmgr_info_cxt(typIoFunc, &cache->proc, fcinfo->flinfo->fn_mcxt);
+
+		if (func == IOFunc_output)
+		{
+			char *t = format_type_be(cache->typId);
+			cache->outString = MemoryContextAlloc(fcinfo->flinfo->fn_mcxt, strlen(t) + 2);
+			sprintf(cache->outString, "(%s,", t);
+		}
+		else
+			cache->outString="\0";
+
+		fcinfo->flinfo->fn_extra = (void *) cache;
+	}
+
+	return cache;
+}
+
+
 Oid
 getIntOid()
 {
+	Oid		out;
 	int		ret;
 	bool	isnull;
+	bool	do_pop = false;
+
+	if (!SPI_connect()) {
+		do_pop = true;
+		SPI_push();
+	}
 
 	/*
 	 * Get OID of our internal data type. This is necessary because record_in and
@@ -252,6 +279,14 @@ getIntOid()
 	if (!(ret = SPI_execute("SELECT '_variant._variant'::regtype::oid", true, 1)))
 		elog( ERROR, "SPI_execute returned %s", SPI_result_code_string(ret));
 
-	return DatumGetObjectId( heap_getattr(SPI_tuptable->vals[0], 1, SPI_tuptable->tupdesc, &isnull) );
+	out = DatumGetObjectId( heap_getattr(SPI_tuptable->vals[0], 1, SPI_tuptable->tupdesc, &isnull) );
+
+	/* Remember this frees everything palloc'd since our connect/push call */
+	if (do_pop)
+		SPI_pop();
+	else
+		SPI_finish();
+
+	return out;
 }
 // vi: noexpandtab sw=2 ts=2
