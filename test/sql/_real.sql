7b6edcd29e1b8cc46ba46cadb8dfd05e55a506f8
Jim Nasby <Jim.Nasby@BlueTreble.com>
2014-12-10 21:04:58 -0600

    Add array number tests

---
+++ test/sql/_real.sql
@@ -0,0 +1,24 @@
+\set ECHO 0
+\set QUIET 1
+
+/*
+ * SEE NOTES IN _text.sql!
+ */
+
+\set baseline_type numeric[]
+-- Space separated string
+\set test_types 'int2[] int4[] int8[] float[] real[] numeric[]'
+\set test_types 'real[]'
+-- Used in array[ :base_values ]
+\set base_values '$${-1.1,-1.1,1.1}$$, $${-1.1,0.1,1.1}$$, $${-1.1,1.1,1.1}$$, $${NULL,-1.1,1.1}$$, $${NULL,0.1,1.1}$$, $${NULL,1.1,1.1}$$, $${-1.1,NULL,1.1}$$, NULL'
+\set compare_value '{-1.1,0.1,1.1}'
+
+
+\i test/helpers/type_setup.sql
+
+SELECT plan(sum(test_count)::int) FROM plan;
+
+\i test/helpers/type.sql
+
+
+-- vi: expandtab sw=2 ts=2
