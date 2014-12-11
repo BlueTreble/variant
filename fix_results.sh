def84c58e54b667ad3673ab7fe7583221620e58b
Jim Nasby <Jim.Nasby@BlueTreble.com>
2014-12-10 22:06:27 -0600

    Improve output of fix_results

--- fix_results.sh
+++ fix_results.sh
@@ -3,7 +3,9 @@
 cmd='^(ok |\set ECHO 0|1\.\.[0-9]+|$)'
 
 if [ -z $(egrep -qv "$cmd" results/*) ]; then
+    echo "No errors found; copying results"
     cp results/*.out test/expected
+    git status -s test/expected
 else
     echo "Errors found:"
     egrep -v "$cmd" results/*
