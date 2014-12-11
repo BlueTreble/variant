1b4180adfae2332f1a6b6941cf4bef9026b1b813
Jim Nasby <Jim.Nasby@BlueTreble.com>
2014-12-10 22:57:08 -0600

    More fix_results improvements

--- fix_results.sh
+++ fix_results.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+results='results/*'
 cmd='^(ok |\\set ECHO 0|1\.\.[0-9]+|$)'
-out=`egrep -v "$cmd" results/*`
+out=`egrep -v "$cmd" $results`
 
 if [ -z "$out" ]; then
     echo "No errors found; copying results $out"
-    cp results/*.out test/expected
+    cp $results.out test/expected
     git status -s test/expected
+
+    # Sanity-check...
+    egrep -i 'not ok|error|#' $results
 else
     echo "Errors found:"
-    egrep -v "$cmd" results/*
+    egrep -v "$cmd" $results
 fi
