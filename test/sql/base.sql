a2c4ff82515d245d622dbd4805f0e118f7efdbf4
Jim Nasby <Jim.Nasby@BlueTreble.com>
2014-12-10 21:51:32 -0600

    Remove number tests from base.sql

--- test/sql/base.sql
+++ test/sql/base.sql
@@ -3,70 +3,6 @@ BEGIN;
 \i test/helpers/tap_setup.sql
 \i test/helpers/common.sql
 
-CREATE TEMP TABLE ncmp_raw(
-	l		variant.variant("test variant")
-	, r		variant.variant("test variant")
-	, lr	int
-	, rr	int
-);
-INSERT INTO ncmp_raw
-SELECT l, r, lr, rr
-FROM (
-	SELECT *
-			/*
-			 * Need to cast lr/rr to text because "blah" isn't actually a valid array and format() gets mad.
-			 */
-			, variant.text_in(format( $$(%s,%s)$$, t, '"' || lr::text || '"' ), 'test variant') AS l
-			, variant.text_in(format( $$(%s,%s)$$, t, '"' || rr::text || '"' ), 'test variant') AS r
-		FROM (
-				SELECT *, 0::numeric AS lr, v::numeric AS rr
-					FROM unnest(array[ '0', '1', '-1', NULL ]) AS vals(v)
-						, unnest( string_to_array( 'int2 int4 int8 float real numeric', ' ' ) ) AS types(t)
-			) a
-	) a
-;
-CREATE OR REPLACE TEMP VIEW ncmp AS
-	SELECT *
-			, variant.text_out(l) AS l_text
-			, variant.text_out(r) AS r_text
-			, coalesce(lr::text, 'NULL') AS lr_text
-			, coalesce(rr::text, 'NULL') AS rr_text
-	FROM ncmp_raw
-;
-CREATE OR REPLACE TEMP VIEW ncmp_ops AS SELECT * FROM ncmp, ops;
-
-CREATE TEMP TABLE _ncmp_raw(
-	l		variant.variant("test variant")
-	, r		variant.variant("test variant")
-	, lr	numeric[]
-	, rr	numeric[]
-);
-INSERT INTO _ncmp_raw
-SELECT l, r, lr, rr
-FROM (
-	SELECT *
-			/*
-			 * Need to cast lr/rr to text because "blah" isn't actually a valid array and format() gets mad.
-			 */
-			, variant.text_in(format( $$(%s[],%s)$$, t, '"' || lr::text || '"' ), 'test variant') AS l
-			, variant.text_in(format( $$(%s[],%s)$$, t, '"' || rr::text || '"' ), 'test variant') AS r
-		FROM (
-				SELECT *, '{0,1,1}'::numeric[] AS lr, v::numeric[] AS rr
-					FROM unnest(array[ '{0,0,1}', '{0,1,1}', '{0,2,1}', '{NULL,1,1}', '{0,NULL,1}', '{0,1,NULL}', NULL ]) AS vals(v)
-						, unnest( string_to_array( 'int2 int4 int8 float real numeric', ' ' ) ) AS types(t)
-			) a
-	) a
-;
-CREATE OR REPLACE TEMP VIEW _ncmp AS
-	SELECT *
-			, variant.text_out(l) AS l_text
-			, variant.text_out(r) AS r_text
-			, coalesce(lr::text, 'NULL') AS lr_text
-			, coalesce(rr::text, 'NULL') AS rr_text
-	FROM _ncmp_raw
-;
-CREATE OR REPLACE TEMP VIEW _ncmp_ops AS SELECT * FROM _ncmp, ops;
-
 CREATE TEMP TABLE box_cmp_raw(
 	l		variant.variant("test variant")
 	, r		variant.variant("test variant")
@@ -109,8 +45,6 @@ SELECT plan( (
 	+3 -- register
 	+6 -- register__get*
 	+4 -- NULL
-	+ (SELECT count(*)::int FROM ncmp_ops)
-	+ (SELECT count(*)::int FROM _ncmp_ops)
 	+ (SELECT count(*)::int FROM box_cmp_ops)
 )::int );
 
@@ -206,33 +140,6 @@ SELECT is( 1::int::variant.variant("test variant")::int = NULL, false, '(int,1):
 SET transform_null_equals = off;
 SELECT is( NULL::int::variant.variant("test variant")::int = NULL, NULL, '(int,)::int = NULL is NULL' );
 SELECT is( 1::int::variant.variant("test variant")::int = NULL, NULL, '(int,1)::int != NULL is NULL' );
-
-SELECT pg_temp.exec_text(
-			$$SELECT is( %s )$$
-			, format( $fmt$variant.text_in(%L, 'test variant') %s variant.text_in(%L, 'test variant')
-					, %s %s %s
-					, $$%L %s %L$$$fmt$
-				, l, op, r
-				, lr_text, op, rr_text
-				, rpad(l_text, len_l), op, rpad(r_text, len_r)
-			)
-		)
-	FROM ncmp_ops
-		, (SELECT max(length(l_text)) AS len_l, max(length(r_text)) AS len_r FROM ncmp) l
-;
-SELECT pg_temp.exec_text(
-			$$SELECT is( %s )$$
-			, format( $fmt$variant.text_in(%L, 'test variant') %s variant.text_in(%L, 'test variant')
-					, %L::numeric[] %s %L::numeric[]
-					, $$%L::numeric[] %s %L::numeric[]$$$fmt$
-				, l, op, r
-				, lr, op, rr
-				, rpad(l_text, len_l), op, rpad(r_text, len_r)
-			)
-		)
-	FROM _ncmp_ops
-		, (SELECT max(length(l_text)) AS len_l, max(length(r_text)) AS len_r FROM _ncmp) l
-;
 
 --SET client_min_messages = debug;
 SELECT pg_temp.exec_text(
