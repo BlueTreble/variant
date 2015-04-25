884f8173496a809cf0aa72357f358402c154e66b
Jim Nasby <Jim.Nasby@BlueTreble.com>
2015-04-24 21:34:46 -0500

    Factor out psql settings

---
+++ test/helpers/psql.sql
@@ -0,0 +1,6 @@
+\pset format unaligned
+\pset tuples_only true
+\pset pager
+-- Revert all changes on failure.
+\set ON_ERROR_ROLLBACK 1
+\set ON_ERROR_STOP true
--- test/helpers/tap_setup.sql
+++ test/helpers/tap_setup.sql
@@ -1,9 +1,3 @@
 \i test/helpers/pgtap-core.sql
 
-\pset format unaligned
-\pset tuples_only true
-\pset pager
--- Revert all changes on failure.
-\set ON_ERROR_ROLLBACK 1
-\set ON_ERROR_STOP true
-
+\i test/helpers/psql.sql
